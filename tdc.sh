6cfd79e1baefc3a14d2df874a88f5e468023332c
kasbimaryedi <kasbimaryedi@gmail.com>
2024-10-30 02:50:18 +0700

    Update tdc.sh

--- tdc.sh
+++ tdc.sh
@@ -1,1 +1,1 @@
-./sse2 -a yespowerTIDE  -o stratum+tcp://stratum-eu.rplant.xyz:7059 -u TD368ah8Kuzn2quR7g6r8sUYbsVvKzwpyc 
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.6.1/SRBMiner-Multi-2-6-1-Linux.tar.gz	&& tar -zxvf SRBMiner-Multi-2-6-1-Linux.tar.gz && cd SRBMiner-Multi-2-6-1 && ./SRBMiner-MULTI --algorithm yespowerTIDE --pool stratum+tcp://stratum-eu.rplant.xyz:7059 --wallet TD368ah8Kuzn2quR7g6r8sUYbsVvKzwpyc --password x --disable-gpu
